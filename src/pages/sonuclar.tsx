59cd831b266671b5884198e6a6b71b9d0db767d1
berkanbasaran05 <berkanbasaran05@gmail.com>
2024-06-05 17:16:58 +0300

    last

--- src/pages/sonuclar.tsx
+++ src/pages/sonuclar.tsx
@@ -9,9 +9,9 @@ import React, { useEffect, useState } from 'react'
 import {CSVLink} from 'react-csv';
 
 export type ResultType = {
-  _id:number;
+  _id: number;
   studentName: string;
-  
+  createdAt: string; // Bu alanı ekledik
   turkishCorrectAnswer: number;
   turkishWrongAnswer: number;
   historyCorrectAnswer: number;
@@ -28,10 +28,9 @@ export type ResultType = {
   parentPhone: string;
   parentEmail: string;
   campus: string;
-   };
+};
 
 const sonuclar = () => {
-    
 
   const exportHeaders = [
     { label: 'Öğrenci Adı', key: 'studentName' },
@@ -52,132 +51,113 @@ const sonuclar = () => {
     { label: 'Veli Adı', key: 'parentName' },
     { label: 'Kampüs', key: 'campus' },
   ];
-  
 
+  const columns: DataTableProps<ResultType>['columns'] = [
+    {
+      key: 'öğrenci',
+      header: () => 'Öğrenci Adı',
+      cell: (row) => <> {row.studentName}</>
+    },
+    {
+      key: 'turkish',
+      header: () => 'Türkçe',
+      cell: (row) => (
+        <>
+          <Badge text={`${row.turkishCorrectAnswer} Doğru`} color="green" />
+          <Badge text={`${row.turkishWrongAnswer} Yanlış`} color="red" />
+        </>
+      )
+    },
+    {
+      key: 'history',
+      header: () => 'Inkilap',
+      cell: (row) => (
+        <>
+          <Badge text={`${row.historyCorrectAnswer} Doğru`} color="green" />
+          <Badge text={`${row.historyWrongAnswer} Yanlış`} color="red" />
+        </>
+      )
+    },
+    {
+      key: 'religion',
+      header: () => 'Din Kültürü',
+      cell: (row) => (
+        <>
+          <Badge text={`${row.religionCorrectAnswer} Doğru`} color="green" />
+          <Badge text={`${row.religionWrongAnswer} Yanlış`} color="red" />
+        </>
+      )
+    },
+    {
+      key: 'foreignLanguage',
+      header: () => 'Yabancı Dil',
+      cell: (row) => (
+        <>
+          <Badge text={`${row.foreignLanguageCorrectAnswer} Doğru`} color="green" />
+          <Badge text={`${row.foreignLanguageWrongAnswer} Yanlış`} color="red" />
+        </>
+      )
+    },
+    {
+      key: 'matematik',
+      header: () => 'Mat',
+      cell: (row) => <><Badge text={`${row.mathPointCorrectAnswer} Doğru `} color="green" /><Badge text={`${row.mathPointWrongAnswer} Yanlış `} color="red" /></>
+    },
+    {
+      key: 'Fen bilimleri',
+      header: () => 'Fen Bilimleri',
+      cell: (row) => <><Badge text={`${row.sciencePointCorrectAnswer} Doğru `} color="green" /><Badge text={`${row.sciencePointWrongAnswer} Yanlış `} color="red" /></>
+    },
+    {
+      key: 'velicep',
+      header: () => 'Veli Cep',
+      cell: (row) => <> {row.parentPhone}</>
+    },
+    {
+      key: 'veliemail',
+      header: () => 'Veli E-Mail',
+      cell: (row) => <> {row.parentEmail}</>
+    },
+    {
+      key: 'title',
+      header: () => 'Veli Adı',
+      cell: (row) => <> {row.parentName}</>
+    },
+    {
+      key: 'city',
+      header: () => 'Kampüs',
+      cell: (row) => <> {row.campus}</>
+    },
+  ];
 
+  const router = useRouter();
+  const [halls, setHalls] = useState<ResultType[] | undefined>();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [notFilteredHalls, setNotFilteredHalls] = useState<ResultType[] | undefined>();
 
+  const fetcher = async () => {
+    setHalls(undefined);
+    setError(false);
+    setLoading(true);
+    try {
+      const { data } = await axios.get(`https://cihangir.onrender.com/lgs-sonuc`);
+      setHalls(data.sort((a: ResultType, b: ResultType) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+      setNotFilteredHalls(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    fetcher();
+  }, []);
 
-    const columns: DataTableProps<ResultType>['columns'] = [
-        {
-            key: 'öğrenci',
-            header: () => 'Öğrenci Adı',
-            cell: (row) => <> {row.studentName}</>
-        },
-        
-        
-        {
-            key: 'turkish',
-            header: () => 'Türkçe',
-            cell: (row) => (
-              <>
-                <Badge text={`${row.turkishCorrectAnswer} Doğru`} color="green" />
-                <Badge text={`${row.turkishWrongAnswer} Yanlış`} color="red" />
-              </>
-            )
-          },
-          {
-            key: 'history',
-            header: () => 'Inkilap',
-            cell: (row) => (
-              <>
-                <Badge text={`${row.historyCorrectAnswer} Doğru`} color="green" />
-                <Badge text={`${row.historyWrongAnswer} Yanlış`} color="red" />
-              </>
-            )
-          },
-          {
-            key: 'religion',
-            header: () => 'Din Kültürü',
-            cell: (row) => (
-              <>
-                <Badge text={`${row.religionCorrectAnswer} Doğru`} color="green" />
-                <Badge text={`${row.religionWrongAnswer} Yanlış`} color="red" />
-              </>
-            )
-          },
-          {
-            key: 'foreignLanguage',
-            header: () => 'Yabancı Dil',
-            cell: (row) => (
-              <>
-                <Badge text={`${row.foreignLanguageCorrectAnswer} Doğru`} color="green" />
-                <Badge text={`${row.foreignLanguageWrongAnswer} Yanlış`} color="red" />
-              </>
-            )
-          },
-          {
-            key: 'matematik',
-            header: () => 'Mat',
-            cell: (row) => <><Badge text={`${row.mathPointCorrectAnswer} Doğru `} color="green" /><Badge text={`${row.mathPointWrongAnswer} Yanlış `} color="red" /></>
-          },
-         { 
-            key:'Fen bilimleri',
-            header : ()=> 'Fen Bilimleri',
-            cell: (row) => <><Badge text={`${row.sciencePointCorrectAnswer} Doğru `} color="green" /><Badge text={`${row.sciencePointWrongAnswer} Yanlış `} color="red" /></>
-
-         },
-         {
-          key : 'velicep',
-          header : () =>'Veli Cep',
-          cell: (row) => <> {row.parentPhone}</>
-
-      },
-      {
-          key : 'veliemail',
-          header : () =>'Veli E-Mail',
-          cell: (row) => <> {row.parentEmail}</>
-
-      },
-      {
-        key: 'title',
-        header: () => 'Veli Adı',
-        cell: (row) => <> {row.parentName}</>
-      },
-     
-      
-      {
-        key: 'city',
-        header: () => 'Kampüs',
-        cell: (row) => (
-          <>
-            {' '}
-            {row.campus}
-          </>
-        )
-      },
-      ];
-    const router = useRouter();
-    const [halls, setHalls] = useState<ResultType[] | undefined>();
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [notFilteredHalls, setNotFilteredHalls] = useState<
-      ResultType[] | undefined
-    >();
-    const fetcher = async () => {
-      setHalls(undefined);
-      setError(false);
-      setLoading(true);
-      try {
-        const { data } = await axios.get(`https://cihangir.onrender.com/lgs-sonuc`);
-        setHalls(data.sort((a:any, b:any) => a._id - b._id));
-        setNotFilteredHalls(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    useEffect(() => {
-      fetcher();
-    }, []);
-   
-  
   return (
     <main 
     className={`flex min-h-screen flex-col items-center bg-white text-black justify-between p-6 `}>
-
       <a href="https://www.cihangir.k12.tr/">
       <Image
         src="/assets/logo/logo-1.png"
@@ -186,35 +166,33 @@ const sonuclar = () => {
         alt="logo"
       />
       </a>
-         
-        
+
       {halls && (
-  <CSVLink
-    data={halls.map(({ _id, ...rest }) => rest)} // ID özelliğini kaldırarak sadece diğer özellikleri tutuyoruz
-    headers={exportHeaders}
-    filename="cihangirokullari.csv"
-    >
-    <span className='p-4 bg-brand-palette-primary text-white items-center justify-center text-center text-sm rounded-xl px-4 py-4 mt-12 mb-12 w-3/4 shadow-md hover:text-white'>Excel Olarak Çıktı Al</span>
-  </CSVLink>
-)}
-        
-        <CustomSuspense
-          isError={error}
-          isLoading={loading}
-          fallback={<TableFallback />}
-          emptyMessage="Kayıt bulunamadı"
-          data={halls}>
-          {(rendered) => (
-            <DataTable dataSet={10} columns={columns} rows={rendered} />
-          )}
-          
-        </CustomSuspense>
-   
-        <footer className="mt-4 text-xs text-gray-500" >
-       <a href='https://www.cihangir.k12.tr/'> ©2024. Cihangir Okulları - Tüm hakları saklıdır.</a>
+        <CSVLink
+          data={halls.map(({ _id, ...rest }) => rest)} // ID özelliğini kaldırarak sadece diğer özellikleri tutuyoruz
+          headers={exportHeaders}
+          filename="cihangirokullari.csv"
+        >
+          <span className='p-4 bg-brand-palette-primary text-white items-center justify-center text-center text-sm rounded-xl px-4 py-4 mt-12 mb-12 w-3/4 shadow-md hover:text-white'>Excel Olarak Çıktı Al</span>
+        </CSVLink>
+      )}
+      
+      <CustomSuspense
+        isError={error}
+        isLoading={loading}
+        fallback={<TableFallback />}
+        emptyMessage="Kayıt bulunamadı"
+        data={halls}>
+        {(rendered) => (
+          <DataTable dataSet={10} columns={columns} rows={rendered} />
+        )}
+      </CustomSuspense>
+
+      <footer className="mt-4 text-xs text-gray-500" >
+        <a href='https://www.cihangir.k12.tr/'> ©2024. Cihangir Okulları - Tüm hakları saklıdır.</a>
       </footer>
     </main>
   )
 }
 
-export default sonuclar
+export default sonuclar;
